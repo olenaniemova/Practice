80970c95067f7492a9cd6ebd60966cbc6bc92f0f
olenaniemova <39510996+olenaniemova@users.noreply.github.com>
2018-05-23 11:15:45 +0300

    Add files via upload

---
+++ Step_0/learn_ruby/08_book_titles/book.rb
@@ -0,0 +1,10 @@
+class Book
+  attr_accessor :title
+
+  def title=(str)
+    arr = ['and', 'over', 'the', 'a', 'an', 'in', 'of']
+    str.capitalize!
+    @title = str.gsub(/\w+/) {|match| arr.include?(match) ? match : match.capitalize}
+  end
+
+end
