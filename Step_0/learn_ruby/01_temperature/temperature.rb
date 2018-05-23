81d6d603913f9f396ac132d2a6446e29a7f0d562
olenaniemova <39510996+olenaniemova@users.noreply.github.com>
2018-05-23 11:04:20 +0300

    Add files via upload

---
+++ Step_0/learn_ruby/01_temperature/temperature.rb
@@ -0,0 +1,12 @@
+#ftoc
+#one degree fahrenheit is 5/9 of one degree celsius
+#c = (f-32)*5/9
+def ftoc (f_temperature)
+  c_temperature = (f_temperature - 32) * (5.0/9.0)
+end
+
+#ctof
+#f = c*9/5 + 32
+def ctof(c_temperature)
+  f_temperature = c_temperature*(9.0/5.0) + 32
+end
