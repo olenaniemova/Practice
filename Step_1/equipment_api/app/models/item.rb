09a6527929f97ba6269d783d19aa939ba000afff
olenaniemova <nemova.elena20@gmail.com>
2018-05-31 14:00:03 +0300

    step 1 refactoring 3

--- Step_1/equipment_api/app/models/item.rb
+++ Step_1/equipment_api/app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_many :written_reviews, as: :reviewable, dependent: :destroy, class_name: "Review"
+  has_many :reviews, as: :reviewable, dependent: :destroy
 end
