ec53de8a81cb9835f4068a596b63247f9aa116a2
olenaniemova <nemova.elena20@gmail.com>
2018-06-14 14:39:21 +0300

    Step 5 refactoring

--- Step_1/equipment_api/app/models/item.rb
+++ Step_1/equipment_api/app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   has_and_belongs_to_many :filter_options
 
   scope :by_title, ->(word) { where('title ~* ?', word) }
-  scope :by_category, ->(id) { joins(:category).where(categories: { id: id }) }
+  scope :by_category, ->(id) { where(category_id: id) }
   scope :by_option, ->(id) { joins(:filter_options).where(filter_options: { id: id })  }
 
   scope :by_price_range, ->(p_start, p_end, days) {
@@ -18,16 +18,7 @@ class Item < ApplicationRecord
     b_start = booking[:start_date].between(d_start..d_end)
     b_end = booking[:end_date].between(d_start..d_end)
     query = booking.project(booking[:item_id]).where(b_start.or(b_end)).distinct.to_sql
-    booking_items = Booking.find_by_sql(query)
-
-    if booking_items.empty?
-      query = Item.arel_table.project(Arel.sql('*'))
-    else
-      b_items = []
-      booking_items.each { |b| b_items.push(b[:item_id]) }
-      query = Item.arel_table[:id].not_eq_all(b_items)
-    end
-    Item.find_by_sql(query)
+    booking_items = Booking.find_by_sql(query).pluck(:item_id)
+    Item.where(Item.arel_table[:id].not_in(booking_items))
   }
-
 end
